373ab56d71b4ad7571a14b93a255b50042faf598
cristian gianoboli <cristian.gianoboli@artekium.com>
2025-12-20 13:23:33 -0300

    create product entitys columns data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -24,7 +25,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       // autoLoadEntities carga automaticamente las entidades
       autoLoadEntities: true,
       synchronize: true
-    })
+    }),
+
+    ProductsModule
   ],
 })
 export class AppModule {}
---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,52 @@
+// Esto representa lo que hay en la base de datos, como una tabla
+
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    // con este decorador vamos creando nuevas columnas
+    @Column('text', {
+        //con esa regla establecemos que no se repita el nombre
+        //de producto en este caso.
+        unique: true
+    })
+    title: string;
+
+    @Column('numeric', {
+        default: 0
+    })
+    price: number;
+
+    @Column({
+        type: 'text',
+        // puede aceptar nulos
+        nullable: true
+    })
+    description: string;
+
+    @Column('text', {
+        unique: true
+    })
+    slug: string;
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number;
+
+    @Column('text', {
+        array: true
+    })
+    sizes: string[];
+
+    @Column('text')
+    gender: string;
+
+    //tags
+
+    //images
+}
