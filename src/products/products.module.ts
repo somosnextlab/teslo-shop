65ba6b1c389270455e3e94b6bb3ce9ced4ccd46d
cristian gianoboli <cristian.gianoboli@artekium.com>
2025-12-24 10:06:47 -0300

    Product image entity

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
+
+import { Product, ProductImage } from './entities';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   imports: [
     // de esta manera importamos la entity de producto creada
-    TypeOrmModule.forFeature([Product])
+    TypeOrmModule.forFeature([Product, ProductImage])
   ]
 })
 export class ProductsModule {}
